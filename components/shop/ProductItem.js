1ca19023311f71b94d65dfb71819ed8ab1ff1d2c
Alexander <alexander.fau.usa@gmail.com>
2021-05-31 23:15:43 -0400

    feat: added to products detail and overview page

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -1,14 +1,85 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import Colors from "../../constants/Colors";
 
 const ProductItem = (props) => {
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <TouchableOpacity onPress={props.onViewDetail}>
+      <View style={styles.product}>
+        <View style={styles.imageContainer}>
+          <Image style={styles.image} source={{ uri: props.image }} />
+        </View>
+        <View style={styles.details}>
+          <Text style={styles.title}>{props.title}</Text>
+          <Text style={styles.price}>${props.price.toFixed(2)}</Text>
+        </View>
+        <View style={styles.actions}>
+          <Button
+            color={Colors.primary}
+            title="View Details"
+            onPress={props.onViewDetail}
+          />
+          <Button
+            color={Colors.primary}
+            title="To Cart"
+            onPress={props.onAddToCart}
+          />
+        </View>
+      </View>
+    </TouchableOpacity>
   );
 };
 
-export default ProductItem;
+const styles = StyleSheet.create({
+  product: {
+    shadowColor: "black",
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    elevation: 5,
+    borderRadius: 10,
+    backgroundColor: "white",
+    height: 300,
+    margin: 20,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+  imageContainer: {
+    width: "100%",
+    height: "60%",
+    borderTopRightRadius: 10,
+    borderTopLeftRadius: 10,
+    overflow: "hidden",
+  },
+  title: {
+    fontSize: 17,
+    marginVertical: 1,
+  },
+  price: {
+    fontSize: 14,
+    color: "#888",
+  },
+  actions: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: "25%",
+    paddingHorizontal: 20,
+  },
+  details: {
+    alignItems: "center",
+    height: "15%",
+    padding: 10,
+  },
+});
 
-const styles = StyleSheet.create({});
+export default ProductItem;
--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,6 +1,8 @@
 import React from "react";
-import { StyleSheet, Text, View, FlatList } from "react-native";
+import { StyleSheet, Text, View, FlatList, Image } from "react-native";
 import { useSelector } from "react-redux";
+
+import ProductItem from "../../components/shop/ProductItem";
 
 const ProductsOverviewScreen = (props) => {
   const products = useSelector((state) => state.products.availableProducts);
@@ -8,7 +10,22 @@ const ProductsOverviewScreen = (props) => {
     <FlatList
       data={products}
       keyExtractor={(item) => item.id}
-      renderItem={(itemData) => <Text>{itemData.item.title}</Text>}
+      renderItem={(itemData) => (
+        <ProductItem
+          image={itemData.item.imageUrl}
+          title={itemData.item.title}
+          price={itemData.item.price}
+          onViewDetail={() => {
+            props.navigation.navigate({
+              routeName: "ProductDetail",
+              params: {
+                clickedProduct: itemData,
+              },
+            });
+          }}
+          onAddToCart={() => {}}
+        />
+      )}
     />
   );
 };
