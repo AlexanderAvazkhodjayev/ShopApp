c68496c30dbc37cd1d11f6169ad843c4b4b0dbec
Alexander <alexander.fau.usa@gmail.com>
2021-05-31 23:20:03 -0400

    new code

---
+++ screens/shop/ProductDetailScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const ProductDetailScreen = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default ProductDetailScreen;
+
+const styles = StyleSheet.create({});
